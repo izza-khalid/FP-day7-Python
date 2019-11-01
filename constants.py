65d2aa08d68a09400fbe0014ab48667a7a98fea2
Ubuntu <ubuntu@ip-172-31-29-95.ec2.internal>
2019-11-01 18:11:32 +0000

    1

---
+++ constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Created by: Izza khalid
+# Created on: september 2019
+# This constants file is for space alien game
+
+# CircuitPython screen size is 160x120 and sprites are 16x16
+SCREEN_X = 160
+SCREEN_Y = 120
+SCREEN_GRID_X = 16
+SCREEN_GRID_Y = 8
+SPRITE_SIZE = 16
+TOTAL_NUMBER_OF_ALIENS = 5
+FPS = 60
+SPRITE_MOVEMENT_SPEED = 1
+
+# using for button state
+button_state = {
+  "button_up": "up",
+  "button_just_pressed": "just pressed",
+  "button_still_pressed": "still pressed",
+  "button_released": "released"
+}
